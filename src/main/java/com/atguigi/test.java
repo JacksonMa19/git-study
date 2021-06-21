5016cc23d818cef985290e60f055a416e1ead075
JacksonMa19 <1101101037@qq.com>
2021-06-21 16:01:00 +0800

    第一次提交

--- src/main/java/com/atguigi/test.java
+++ src/main/java/com/atguigi/test.java
@@ -1,7 +1,13 @@
 package com.atguigi;
 
+import java.util.HashMap;
+
 public class test {
     public static void main(String[] args) {
+        int i = 0 ;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
         System.out.println("hello");
+
     }
 }
